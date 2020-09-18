dcfdd2ac5413b3b5e423a1e27d572cbb8db20ce4
Alexander Römer <alex@roemer.design>
2020-09-18 22:08:48 +0200

    initial commit

--- LoadingSpinner/ContentView.swift
+++ LoadingSpinner/ContentView.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isSpinning = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            
+            Color(.black)
+                .edgesIgnoringSafeArea(.all)
+            
+            Text("Loading")
+                .foregroundColor(Color(.systemYellow))
+                .shadow(color: Color(.systemYellow), radius: 5, x: 1, y: 1)
+            
+            Circle()
+                .stroke(lineWidth: 5)
+                .frame(width: 150, height: 150)
+                .foregroundColor(Color(.darkGray))
+            
+            Circle()
+                .trim(from: 0, to: 1/4)
+                .stroke(style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+                .frame(width: 150, height: 150)
+                .foregroundColor(Color(.systemYellow))
+                .overlay(Circle()
+                            .frame(width: 15, height: 15)
+                            .foregroundColor(Color(.systemYellow))
+                            .offset(x: 0, y: 75)
+                )
+                .shadow(color: Color(.yellow), radius: 5, x: 1, y: 1)
+                .offset()
+                .rotationEffect(.degrees(isSpinning ? 360 : 0))
+                .animation(Animation.linear(duration: 2).repeatForever(autoreverses: false))
+                .onAppear() {
+                    self.isSpinning.toggle()
+                }
+        }
+        
     }
 }
 
