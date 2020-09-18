e08c5ee11f3e7bf31926b8e38d2edf84995fff3c
Alexander Römer <alex@roemer.design>
2020-09-18 21:41:45 +0200

    Initial Commit

---
+++ LoadingSpinner/LoadingSpinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingSpinnerApp.swift
+//  LoadingSpinner
+//
+//  Created by Alexander Römer on 18.09.20.
+//
+
+import SwiftUI
+
+@main
+struct LoadingSpinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
